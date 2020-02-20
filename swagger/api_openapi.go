bb6e17e47100f179030c88c6e90be0f7e28c47b9
Per von Rosen <per.von.rosen@gmail.com>
2020-02-20 14:12:13 +0100

    new routes

--- swagger/api_openapi.go
+++ swagger/api_openapi.go
@@ -16,24 +16,10 @@ import (
 	"os"
 )
 
-func Openapi_yaml(w http.ResponseWriter, r *http.Request)
-{
-	Openapi_option(w http.ResponseWriter, r *http.Request, true)
-}
-
-func Openapi_json(w http.ResponseWriter, r *http.Request)
-{
-	Openapi_option(w http.ResponseWriter, r *http.Request, false)
-}
-
-/* Openapi - here we can retrieve the openapi3.0 aka "swagger file" for auto-provision*/
-func Openapi_option(w http.ResponseWriter, r *http.Request, yaml bool=true) {
-	if yaml==true 
-		w.Header().Set("Content-Type", "application/yaml; charset=UTF-8")
-		file, err := os.Open("/openapi.yaml")
-	else
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		file, err := os.Open("/openapi.json")
+/* Openapi_yaml - here we can retrieve the openapi3.0 aka "swagger file" for auto-provision*/
+func Openapi_yaml(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/yaml; charset=UTF-8")
+	file, err := os.Open("/openapi.yaml")
 	defer file.Close()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -49,6 +35,27 @@ func Openapi_option(w http.ResponseWriter, r *http.Request, yaml bool=true) {
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write(filecontent)
-	//fmt.Fprintf(w, string(filecontent))
+
 }
 
+/* Openapi_json - here we can retrieve the openapi3.0 aka "swagger file" for auto-provision*/
+func Openapi_json(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	file, err := os.Open("/openapi.json")
+	defer file.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Could not find openapi file")
+		return
+	}
+
+	filecontent, err := ioutil.ReadAll(file)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Could not read openapi file after loading it")
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(filecontent)
+
+}
