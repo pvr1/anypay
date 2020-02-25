34ca8f1dde9b1d6aa406929be73e35468a6e03c5
Per von Rosen <per.von.rosen@gmail.com>
2020-02-25 22:16:01 +0100

    aasas

--- go/api_fxorders.go
+++ go/api_fxorders.go
@@ -116,20 +116,20 @@ func AddFxorder(c *gin.Context) {
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
-	brokers := strings.Split(kafkaBrokerURL, ",")
+	//brokers := strings.Split(kafkaBrokerURL, ",")
 
 	// make a new reader that consumes from topic-A
-	config := kafka.ReaderConfig{
-		Brokers:         brokers,
-		GroupID:         kafkaClientID,
-		Topic:           kafkaTopicIn,
-		MinBytes:        10e3,            // 10KB
-		MaxBytes:        10e6,            // 10MB
-		MaxWait:         1 * time.Second, // Maximum amount of time to wait for new data to come when fetching batches of messages from kafka.
-		ReadLagInterval: -1,
-	}
+	// config := kafka.ReaderConfig{
+	// Brokers:         brokers,
+	// GroupID:         kafkaClientID,
+	// Topic:           kafkaTopicIn,
+	// MinBytes:        10e3,            // 10KB
+	// MaxBytes:        10e6,            // 10MB
+	// MaxWait:         1 * time.Second, // Maximum amount of time to wait for new data to come when fetching batches of messages from kafka.
+	// ReadLagInterval: -1,
+	// }
 
-	reader := kafka.NewReader(config)
+	//reader := kafka.NewReader(config)
 
 	// connect to kafka
 	kafkaProducer, err := kafkaUtils.Configure(strings.Split(kafkaBrokerURL, ","), kafkaClientID, kafkaTopicOut)
@@ -139,22 +139,22 @@ func AddFxorder(c *gin.Context) {
 	}
 
 	defer kafkaProducer.Close()
-	defer reader.Close()
+	//defer reader.Close()
 
-	log.Debug().Msg("Conected to kafka")
+	//log.Debug().Msg("Conected to kafka")
 
-	m, err := reader.ReadMessage(context.Background())
-
-	log.Debug().Msg("Read request message body")
-	if err != nil {
-		log.Error().Msgf("error while receiving message: %s", err.Error())
-	}
-
-	log.Debug().Msg("Inside for loop...")
-	value := m.Value
-
-	log.Debug().Msg("Got a Message")
-	//		if m.CompressionCodec == snappy.NewCompressionCodec() {
+	// m, err := reader.ReadMessage(context.Background())
+	//
+	// log.Debug().Msg("Read request message body")
+	// if err != nil {
+	// log.Error().Msgf("error while receiving message: %s", err.Error())
+	// }
+	//
+	// log.Debug().Msg("Inside for loop...")
+	// value := m.Value
+	//
+	// log.Debug().Msg("Got a Message")
+	// if m.CompressionCodec == snappy.NewCompressionCodec() {
 	//			_, err = snappy.NewCompressionCodec().Decode(value, m.Value)
 	//		}
 
@@ -179,7 +179,7 @@ func AddFxorder(c *gin.Context) {
 		log.Error().Msgf("error while receiving message: %s", err.Error())
 	}
 	log.Debug().Msg("Printing response from kafka")
-	fmt.Printf("message at topic/partition/offset %v/%v/%v: %s\n", m.Topic, m.Partition, m.Offset, string(value))
+	//fmt.Printf("message at topic/partition/offset %v/%v/%v: %s\n", m.Topic, m.Partition, m.Offset, string(value))
 
 	log.Debug().Msgf("Closing Down")
 
@@ -188,20 +188,94 @@ func AddFxorder(c *gin.Context) {
 
 // DeleteFxorder - Deletes a payment-instruction not settled yet
 func DeleteFxorder(c *gin.Context) {
+	if kafkaBrokerURL == "" {
+		//flag.StringVar(&kafkaBrokerURL, "kafka-brokers", "localhost:19092,localhost:29092,localhost:39092", "Kafka brokers in comma separated value")
+		flag.StringVar(&kafkaBrokerURL, "kafka-brokers", "localhost:9092", "Kafka brokers in comma separated value")
+		flag.BoolVar(&kafkaVerbose, "kafka-verbose", true, "Kafka verbose logging")
+		flag.StringVar(&kafkaTopicIn, "kafka-topicIn", "foo2", "Kafka topic. Only one topic per worker.")
+		flag.StringVar(&kafkaTopicOut, "kafka-topicOut", "foo2", "Kafka topic. Only one topic per worker.")
+		flag.StringVar(&kafkaConsumerGroup, "kafka-consumer-group", "consumer-group", "Kafka consumer group")
+		flag.StringVar(&kafkaClientID, "kafka-client-id", "my-client-id", "Kafka client id")
+	}
 	c.String(http.StatusOK, "FX Order deleted. You were so close...\n")
 }
 
 // GetFxorders - Get all non-matched FX orders that the login has privileges to get
 func GetFxorders(c *gin.Context) {
+	if kafkaBrokerURL == "" {
+		//flag.StringVar(&kafkaBrokerURL, "kafka-brokers", "localhost:19092,localhost:29092,localhost:39092", "Kafka brokers in comma separated value")
+		flag.StringVar(&kafkaBrokerURL, "kafka-brokers", "localhost:9092", "Kafka brokers in comma separated value")
+		flag.BoolVar(&kafkaVerbose, "kafka-verbose", true, "Kafka verbose logging")
+		flag.StringVar(&kafkaTopicIn, "kafka-topicIn", "foo2", "Kafka topic. Only one topic per worker.")
+		flag.StringVar(&kafkaTopicOut, "kafka-topicOut", "foo2", "Kafka topic. Only one topic per worker.")
+		flag.StringVar(&kafkaConsumerGroup, "kafka-consumer-group", "consumer-group", "Kafka consumer group")
+		flag.StringVar(&kafkaClientID, "kafka-client-id", "my-client-id", "Kafka client id")
+	}
+
+	flag.Parse()
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	brokers := strings.Split(kafkaBrokerURL, ",")
+
+	// make a new reader that consumes from topic-A
+	config := kafka.ReaderConfig{
+		Brokers:         brokers,
+		GroupID:         kafkaClientID,
+		Topic:           kafkaTopicIn,
+		MinBytes:        10e3,            // 10KB
+		MaxBytes:        10e6,            // 10MB
+		MaxWait:         1 * time.Second, // Maximum amount of time to wait for new data to come when fetching batches of messages from kafka.
+		ReadLagInterval: -1,
+	}
+
+	reader := kafka.NewReader(config)
+	defer reader.Close()
+
+	log.Debug().Msg("Conected to kafka")
+
+	m, err := reader.ReadMessage(context.Background())
+
+	log.Debug().Msg("Read request message body")
+	if err != nil {
+		log.Error().Msgf("error while receiving message: %s", err.Error())
+	}
+
+	log.Debug().Msg("Inside for loop...")
+
+	log.Debug().Msg("Got a Message")
+
+	fmt.Printf("message at topic/partition/offset %v/%v/%v: %s\n", m.Topic, m.Partition, m.Offset, string(m.Value))
 	c.String(http.StatusOK, "Get FX Order list. Offer:100MM EUR/SEK @ 10.53 , BID:100MM EUR/SEK @ 10.51 . \n")
 }
 
 // GetFxorder - Get a specific foreign exchange order (FX order) with details
 func GetFxorder(c *gin.Context) {
+	if kafkaBrokerURL == "" {
+		//flag.StringVar(&kafkaBrokerURL, "kafka-brokers", "localhost:19092,localhost:29092,localhost:39092", "Kafka brokers in comma separated value")
+		flag.StringVar(&kafkaBrokerURL, "kafka-brokers", "localhost:9092", "Kafka brokers in comma separated value")
+		flag.BoolVar(&kafkaVerbose, "kafka-verbose", true, "Kafka verbose logging")
+		flag.StringVar(&kafkaTopicIn, "kafka-topicIn", "foo2", "Kafka topic. Only one topic per worker.")
+		flag.StringVar(&kafkaTopicOut, "kafka-topicOut", "foo2", "Kafka topic. Only one topic per worker.")
+		flag.StringVar(&kafkaConsumerGroup, "kafka-consumer-group", "consumer-group", "Kafka consumer group")
+		flag.StringVar(&kafkaClientID, "kafka-client-id", "my-client-id", "Kafka client id")
+	}
+
 	c.String(http.StatusOK, "Get specific FX Order status. You are best offer: 100MM EUR/SEK @ 10.53 . \n")
 }
 
 // UpdateFxorder - Update a existing  FX order not matched yet, on the market place
 func UpdateFxorder(c *gin.Context) {
+	if kafkaBrokerURL == "" {
+		//flag.StringVar(&kafkaBrokerURL, "kafka-brokers", "localhost:19092,localhost:29092,localhost:39092", "Kafka brokers in comma separated value")
+		flag.StringVar(&kafkaBrokerURL, "kafka-brokers", "localhost:9092", "Kafka brokers in comma separated value")
+		flag.BoolVar(&kafkaVerbose, "kafka-verbose", true, "Kafka verbose logging")
+		flag.StringVar(&kafkaTopicIn, "kafka-topicIn", "foo2", "Kafka topic. Only one topic per worker.")
+		flag.StringVar(&kafkaTopicOut, "kafka-topicOut", "foo2", "Kafka topic. Only one topic per worker.")
+		flag.StringVar(&kafkaConsumerGroup, "kafka-consumer-group", "consumer-group", "Kafka consumer group")
+		flag.StringVar(&kafkaClientID, "kafka-client-id", "my-client-id", "Kafka client id")
+	}
+
 	c.String(http.StatusOK, "FX Order updated.")
 }
